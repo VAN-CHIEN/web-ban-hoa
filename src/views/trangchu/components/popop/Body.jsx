624521304afde9e9cd980ef41b14c6a63160b436
Văn Chiến <91095947+VAN-CHIEN@users.noreply.github.com>
2022-07-06 23:04:23 +0700

    .

--- src/views/trangchu/components/popop/Body.jsx
+++ src/views/trangchu/components/popop/Body.jsx
@@ -1,11 +1,44 @@
-import React from "react";
+import React, { useState } from "react";
 import Slide1 from '../../img/hoaslide1.jpg';
+import { Card } from 'antd';
 import '../../style/Style.css';
 
 export function Body() {
+    const [activeTabKey1, setActiveTabKey1] = useState('tab1');
+    const onTab1Change = (key) => {
+        setActiveTabKey1(key);
+      };
+      const contentList = {
+        tab1: <p>content1</p>,
+        tab2: <p>content2</p>,
+      };
+      const tabList = [
+        {
+          key: 'tab1',
+          tab: 'tab1',
+        },
+        {
+          key: 'tab2',
+          tab: 'tab2',
+        },
+      ];
     return(
-        <div class="list-images">
-            <img src={Slide1} alt="" class="active" />
-        </div> 
+        <div className="box-body">
+            <div class="list-images">
+                <img src={Slide1} alt="" class="active" />
+            </div> 
+            <Card
+                className="card-list"
+                style={{ width: '100%' }}
+                title="Card title"
+                tabList={tabList}
+                activeTabKey={activeTabKey1}
+                onTabChange={key => {
+                onTab1Change(key);
+                }}
+            >
+                {contentList[activeTabKey1]}
+            </Card>
+        </div>
     )
 }
