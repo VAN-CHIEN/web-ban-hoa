408584e5426a3495f5e8cc3efacbaf1912263281
Văn Chiến <91095947+VAN-CHIEN@users.noreply.github.com>
2022-07-08 18:32:05 +0700

    .

--- src/views/trangchu/components/popop/Header.jsx
+++ src/views/trangchu/components/popop/Header.jsx
@@ -10,31 +10,29 @@ export function HeaderKey(){
     return(
         <Row
         className="grid-header"
-        style={{borderBottom: '2px solid black'}}
+        style={{borderBottom: '2px solid #D22880'}}
         >
             <Col span={7}>
                 <Row>
                     <Col span={12}><img src={logo} alt="ảnh logo"/></Col>
-                    <Col span={12}><h1 style={{marginTop: '20%'}}>Mùa hạ</h1></Col>
+                    <Col span={12}><h1 style={{marginTop: '20%', fontFamily: 'Bookman', fontSize: '28px', color: '#D22880'}}>Mùa Hạ</h1></Col>
                 </Row>
             </Col>
             <Col span={10}>
                 <Breadcrumb style={{marginTop: '10%'}}>
-                    <Breadcrumb.Item><a href="/">Home</a></Breadcrumb.Item>
+                    <Breadcrumb.Item><a style={{color: '#D22880'}} href="/">Home</a></Breadcrumb.Item>
                     <Breadcrumb.Item>
-                        <a href="/">Các loại hoa</a>
+                        <a style={{color: '#D22880'}} href="/">Oder</a>
                     </Breadcrumb.Item>
-                    <Breadcrumb.Item>
-                        <a href="/">Oder</a>
-                    </Breadcrumb.Item>
-                    <Breadcrumb.Item><a href="/">Giỏ hàng</a></Breadcrumb.Item>
+                    <Breadcrumb.Item><a style={{color: '#D22880'}} href="/">Giỏ hàng</a></Breadcrumb.Item>
+                    <Breadcrumb.Item><a style={{color: '#D22880'}} href="/">Hỗ trợ</a></Breadcrumb.Item>
                 </Breadcrumb>
             </Col>
             <Col span={7}>
                 <Row style={{marginTop: '15%'}}>
                     <Col span={11}><Input.Search allowClear placeholder="Nhập loại hoa cần tìm kiếm" /></Col>
                     <Col span={2} />
-                    <Col span={11}><Button type="primary">Đăng nhập</Button></Col>
+                    <Col span={11}><Button style={{backgroundColor: '#D22880'}} type="primary">Đăng nhập</Button></Col>
                 </Row>
                 
             </Col>
